be5437bbea3fdef5be07adae585afa9646a2ef41
Sombat Mayer <sorsadhu@gmail.com>
2022-10-12 10:42:19 -0600

    created a post component

--- src/components/recentPost.js
+++ src/components/recentPost.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-
 import { connect } from "react-redux";
-
 import * as actions from "../actions";
+import Post from "./post";
 
 class RecentPosts extends Component {
 
@@ -15,9 +14,7 @@ class RecentPosts extends Component {
 
             if (index < 3) {
                 return (
-                    <li key={index}>
-                        {post.title}
-                    </li>
+                    <Post {...post} key={index} />
                 )
             }
 
